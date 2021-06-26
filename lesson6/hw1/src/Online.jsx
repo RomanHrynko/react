a69cfe96a96aff973e259e9193f6a5d277fd61e6
Roman Hrynko <romanhrynkoua@gmail.com>
2021-06-26 14:36:12 +0300

    lesson6-hw1-v2

--- lesson6/hw1/src/Online.jsx
+++ lesson6/hw1/src/Online.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Online = () => {
   return (
     <div className="status">
-      <span className="status__text">All good</span>
+      <span className="status__text">All good!</span>
     </div>
   );
 };
