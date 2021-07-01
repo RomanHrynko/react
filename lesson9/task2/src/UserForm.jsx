0f5238ace0417f1cd8fd17659a9d00c8c63f5086
Roman Hrynko <romanhrynkoua@gmail.com>
2021-07-01 13:39:05 +0300

    lesson9-task2-v3

--- lesson9/task2/src/UserForm.jsx
+++ lesson9/task2/src/UserForm.jsx
@@ -26,6 +26,7 @@ class UserForm extends Component {
   handleSubmit = event => {
     event.preventDefault();
     console.log(this.state);
+    this.props.onSubmit(this.state);
   };
 
   render() {
