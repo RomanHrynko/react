828743299dc42891af1d087884b8a51014609d42
Roman Hrynko <romanhrynkoua@gmail.com>
2021-07-06 14:57:44 +0300

    lesson11-task1-v2

--- lesson11/task1/src/Numbers.jsx
+++ lesson11/task1/src/Numbers.jsx
@@ -1,4 +1,5 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 
 class Numbers extends PureComponent {
   render() {
@@ -10,5 +11,14 @@ class Numbers extends PureComponent {
     );
   }
 }
+
+Numbers.propTypes = {
+  title: PropTypes.string,
+  number: PropTypes.number.isRequired,
+};
+
+Numbers.defaultProps = {
+  title: 'Some number',
+};
 
 export default Numbers;
