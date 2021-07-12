4850ade7646c3333318ef07d74c8dc8159739baf
Roman Hrynko <romanhrynkoua@gmail.com>
2021-07-12 16:45:46 +0300

    lesson14-task3-v4

--- lesson14/task3/src/Dimensions.jsx
+++ lesson14/task3/src/Dimensions.jsx
@@ -1,24 +1,15 @@
 import React, { useEffect, useState } from 'react';
 
 const Dimension = () => {
-  const [dimensions, setDimensions] = useState({
-    width: null,
-    height: null,
-  });
+  const [dimensions, setDimensions] = useState({ width: null, height: null });
 
   useEffect(() => {
     const { innerHeight, innerWidth } = window;
-    setDimensions({
-      width: innerWidth,
-      height: innerHeight,
-    });
+    setDimensions({ width: innerWidth, height: innerHeight });
 
-    const handleResize = event => {
-      const { innerHeight, innerWidth } = event.target;
-      setDimensions({
-        width: innerWidth,
-        height: innerHeight,
-      });
+    const handleResize = e => {
+      const { innerHeight, innerWidth } = e.target;
+      setDimensions({ width: innerWidth, height: innerHeight });
     };
     window.addEventListener('resize', handleResize);
 
@@ -28,8 +19,7 @@ const Dimension = () => {
   }, []);
 
   const { width, height } = dimensions;
-
-  return <div className="dimensions">{`${width}px x ${height}px`}</div>;
+  return <div className="dimensions">{`${width}px - ${height}px`}</div>;
 };
 
 export default Dimension;
