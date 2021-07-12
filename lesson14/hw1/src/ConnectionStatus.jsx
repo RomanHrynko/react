0688ea0361cc3d745bbe02349e5d215752d20bed
Roman Hrynko <romanhrynkoua@gmail.com>
2021-07-12 18:16:53 +0300

    lesson14-hw1-v2

--- lesson14/hw1/src/ConnectionStatus.jsx
+++ lesson14/hw1/src/ConnectionStatus.jsx
@@ -3,15 +3,15 @@ import React, { useEffect, useState } from 'react';
 const ConnectionStatus = () => {
   const [status, setStatus] = useState(true);
 
-  const onlineStatusHandler = () => {
-    setStatus(true);
-  };
-
-  const offlineStatusHandler = () => {
-    setStatus(false);
-  };
-
   useEffect(() => {
+    const onlineStatusHandler = () => {
+      setStatus(true);
+    };
+
+    const offlineStatusHandler = () => {
+      setStatus(false);
+    };
+
     window.addEventListener('online', onlineStatusHandler);
     window.addEventListener('offline', offlineStatusHandler);
 
