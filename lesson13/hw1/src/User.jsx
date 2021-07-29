adc224e77b3609f9868bb1a48358ea85c2ea88d5
Roman Hrynko <romanhrynkoua@gmail.com>
2021-07-29 13:11:56 +0300

    last

--- lesson13/hw1/src/User.jsx
+++ lesson13/hw1/src/User.jsx
@@ -1,19 +1,80 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 const User = () => {
+  const { userId } = useParams();
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${userId}`)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error();
+      })
+      .then(({ avatar_url, name, location }) => {
+        setUserData({
+          userName: name,
+          userLocation: location,
+          userAvatar: avatar_url,
+        });
+      });
+  }, [userId]);
+
+  if (!userData) {
+    return null;
+  }
+
+  const { userName, userAvatar, userLocation } = userData;
+
   return (
     <div className="user">
-      <img
-        alt="User Avatar"
-        src="https://avatars1.githubusercontent.com/u/9919?v=4"
-        className="user__avatar"
-      />
+      <img alt="User Avatar" src={userAvatar} className="user__avatar" />
       <div className="user__info">
-        <span className="user__name">GitHub</span>
-        <span className="user__location">San Francisco,CA</span>
+        <span className="user__name">{userName}</span>
+        <span className="user__location">{userLocation}</span>
       </div>
     </div>
   );
 };
+
+// class User extends React.Component {
+//   // state = {
+//   //   userName: null,
+//   //   userAvatar: null,
+//   //   userLocation: null,
+//   // };
+
+//   componentDidMount() {
+//     this.fetchUserData(this.props.match.params.userId);
+//   }
+
+//   componentDidUpdate(prevProps) {
+//     const prevUser = prevProps.match.params.userId;
+//     const currentUser = this.props.match.params.userId;
+
+//     if (prevUser !== currentUser) {
+//       this.fetchUserData(currentUser);
+//     }
+//   }
+
+//   render() {
+//     const { userAvatar, userName, userLocation } = this.state;
+//     if (!userAvatar || !userName || !userLocation) {
+//       return null;
+//     }
+
+//     return (
+//       <div className="user">
+//         <img alt="User Avatar" src={this.state.userAvatar} className="user__avatar" />
+//         <div className="user__info">
+//           <span className="user__name">{this.state.userName}</span>
+//           <span className="user__location">{this.state.userLocation}</span>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
 export default User;
